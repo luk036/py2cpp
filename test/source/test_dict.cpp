f765f6e5234e41e921ecc17eef5515f9a8e04556
Wai-Shing Luk <luk036@gmail.com>
2022-01-10 20:01:34 +0800

    fixed by fix_includes.py and iwyu

--- test/source/test_dict.cpp
+++ test/source/test_dict.cpp
@@ -1,6 +1,7 @@
 #include <doctest/doctest.h>  // for ResultBuilder, TestCase, CHECK, TEST_CASE
 
-#include <py2cpp/dict.hpp>  // for dict
+#include <py2cpp/dict.hpp>  // for dict, key_iterator
+#include <unordered_map>    // for operator!=, __hash_map_const_iterator
 #include <utility>          // for pair
 
 TEST_CASE("Test set") {
--- test/source/test_set.cpp
+++ test/source/test_set.cpp
@@ -1,6 +1,6 @@
-#include <doctest/doctest.h>
+#include <doctest/doctest.h>  // for ResultBuilder, TestCase, CHECK, TEST_CASE
 
-#include <py2cpp/set.hpp>
+#include <py2cpp/set.hpp>  // for set
 
 TEST_CASE("Test set") {
     const auto S = py::set<int>{1, 3, 4, 5, 1};
