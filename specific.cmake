d5befd9d6ee0dc1c286ab73dc4578975624cb78b
Wai-Shing Luk <luk036@gmail.com>
2023-02-15 11:30:26 +0800

    apply fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,12 +1,8 @@
-cpmaddpackage(
-  NAME
-  fmt
-  GIT_TAG
-  7.1.3
-  GITHUB_REPOSITORY
-  fmtlib/fmt
-  OPTIONS
-  "FMT_INSTALL YES" # create an installable target
+CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
 set(SPECIFIC_LIBS fmt::fmt)
