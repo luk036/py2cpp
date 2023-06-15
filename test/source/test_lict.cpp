3fd6b656656aa3d2b33d8b5ed613b9e08daa37a2
Wai-Shing Luk <luk036@gmail.com>
2023-06-15 15:35:03 +0800

    apply fix-format

--- test/source/test_lict.cpp
+++ test/source/test_lict.cpp
@@ -1,16 +1,16 @@
 #include <doctest/doctest.h> // for ResultBuilder, TestCase, CHECK, TEST_CASE
 
-#include <py2cpp/lict.hpp>  // for Lict, key_iterator
+#include <py2cpp/lict.hpp> // for Lict, key_iterator
 #include <vector>
 
 TEST_CASE("Test Lict") {
-    const auto S = py::Lict<double>{std::vector<double>{0.6, 0.7, 0.8}};
-    CHECK_EQ(S.size(), 3U);
-    CHECK(S.contains(1));
-    auto count = 0;
-    for (const auto& p : S) {
-        static_assert(sizeof(p) >= 0, "make compiler happy");
-        ++count;
-    }
-    CHECK(count == 3);
+  const auto S = py::Lict<double>{std::vector<double>{0.6, 0.7, 0.8}};
+  CHECK_EQ(S.size(), 3U);
+  CHECK(S.contains(1));
+  auto count = 0;
+  for (const auto &p : S) {
+    static_assert(sizeof(p) >= 0, "make compiler happy");
+    ++count;
+  }
+  CHECK(count == 3);
 }
