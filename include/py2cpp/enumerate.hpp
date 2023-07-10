fcb306a26996eac4c153260c9eff5cda245ae25b
Wai-Shing Luk <luk036@gmail.com>
2023-07-10 10:50:27 +0000

    update documentation

--- include/py2cpp/enumerate.hpp
+++ include/py2cpp/enumerate.hpp
@@ -10,7 +10,11 @@ namespace py {
 namespace detail {
 
 /**
- * @brief
+ * @brief EnumerateIterator
+ *
+ * The code defines a struct called `EnumerateIterator` that is used to iterate
+ * over a container or range and provide the index of each element in the
+ * iteration.
  *
  * @tparam T
  */
@@ -22,7 +26,14 @@ template <typename T> struct EnumerateIterator {
   TIter iter;
 
   /**
-   * @brief
+   * @brief Not equal to
+   *
+   * The `operator!=` function is an overloaded operator that checks for
+   * inequality between two `EnumerateIterator` objects. It compares the `iter`
+   * member of the current object with the `iter` member of the `other` object.
+   * If they are not equal, it returns `true`, indicating that the two iterators
+   * are not pointing to the same element. Otherwise, it returns `false`,
+   * indicating that the two iterators are equal.
    *
    * @param other
    * @return true
@@ -35,6 +46,12 @@ template <typename T> struct EnumerateIterator {
   /**
    * @brief
    *
+   * The `operator++()` function is an overloaded operator that increments the
+   * `EnumerateIterator` object. It increases the value of the `i` member
+   * variable by 1 and advances the `iter` member variable to the next element
+   * in the iteration. It then returns a reference to the updated
+   * `EnumerateIterator` object.
+   *
    * @return EnumerateIterator&
    */
   EnumerateIterator &operator++() {
@@ -46,6 +63,13 @@ template <typename T> struct EnumerateIterator {
   /**
    * @brief
    *
+   * The `operator*()` function is an overloaded operator that returns the
+   * current element in the iteration as a `std::pair<size_t, iter_ref>`. The
+   * `size_t` value represents the index of the element, and the `iter_ref`
+   * value represents a reference to the element itself. This allows you to
+   * access both the index and the element in a single expression when using the
+   * `enumerate()` function.
+   *
    * @return std::pair<size_t, iter_ref>
    */
   auto operator*() -> std::pair<size_t, iter_ref> {
@@ -54,7 +78,11 @@ template <typename T> struct EnumerateIterator {
 };
 
 /**
- * @brief
+ * @brief EnumerateIterableWrapper
+ *
+ * The code defines a struct called `EnumerateIterableWrapper` that acts as a
+ * wrapper for an iterable object. It provides two member functions, `begin()`
+ * and `end()`, which return instances of the `EnumerateIterator` struct.
  *
  * @tparam T
  */
@@ -62,7 +90,12 @@ template <typename T> struct EnumerateIterableWrapper {
   T &iterable;
 
   /**
-   * @brief
+   * @brief begin
+   *
+   * The `begin()` function is a member function of the
+   * `EnumerateIterableWrapper` struct. It returns an instance of the
+   * `EnumerateIterator<T>` struct, which is used to iterate over the elements
+   * of the iterable object.
    *
    * @return EnumerateIterator<T>
    */
@@ -71,7 +104,12 @@ template <typename T> struct EnumerateIterableWrapper {
   }
 
   /**
-   * @brief
+   * @brief end
+   *
+   * The `end()` function is a member function of the `EnumerateIterableWrapper`
+   * struct. It returns an instance of the `EnumerateIterator<T>` struct, which
+   * is used to mark the end of the iteration over the elements of the iterable
+   * object.
    *
    * @return EnumerateIterator<T>
    */
--- include/py2cpp/range.hpp
+++ include/py2cpp/range.hpp
@@ -29,7 +29,12 @@ template <typename T> struct RangeIterator {
   T i;
 
   /**
-   * @brief
+   * @brief Not equal to
+   *
+   * The `operator!=` function is an overloaded operator that checks if the
+   * current iterator is not equal to another iterator. It takes another
+   * `RangeIterator` object `other` as a parameter and returns a boolean value
+   * indicating whether the two iterators are not equal.
    *
    * @param other
    * @return true
@@ -40,7 +45,12 @@ template <typename T> struct RangeIterator {
   }
 
   /**
-   * @brief
+   * @brief Equal to operator
+   *
+   * The `operator==` function is an overloaded operator that checks if the
+   * current iterator is equal to another iterator. It takes another
+   * `RangeIterator` object `other` as a parameter and returns a boolean value
+   * indicating whether the two iterators are equal.
    *
    * @param other
    * @return true
@@ -53,6 +63,10 @@ template <typename T> struct RangeIterator {
   /**
    * @brief
    *
+   * The code snippet you provided is defining the `operator*()` function for
+   * the `RangeIterator` struct. This function is used to dereference the
+   * iterator and return the value it points to.
+   *
    * @return const_reference
    */
   CONSTEXPR14 auto operator*() const -> const_reference { return this->i; }
@@ -60,12 +74,20 @@ template <typename T> struct RangeIterator {
   /**
    * @brief
    *
+   * The code snippet you provided is defining the `operator*()` function for
+   * the `RangeIterator` struct. This function is used to dereference the
+   * iterator and return the value it points to.
+   *
    * @return reference
    */
   CONSTEXPR14 auto operator*() -> reference { return this->i; }
 
   /**
    * @brief
+   *
+   * The code snippet you provided is defining the pre-increment operator
+   * (`operator++()`) for the `RangeIterator` struct. This operator is used to
+   * increment the iterator to the next element in the range.
    *
    * @return RangeIterator&
    */
@@ -76,6 +98,11 @@ template <typename T> struct RangeIterator {
 
   /**
    * @brief
+   *
+   * The code snippet you provided is defining the post-increment operator
+   * (`operator++(int)`) for the `RangeIterator` struct. This operator is used
+   * to increment the iterator to the next element in the range, but it returns
+   * a copy of the iterator before the increment.
    *
    * @return RangeIterator
    */
@@ -88,6 +115,10 @@ template <typename T> struct RangeIterator {
 
 /**
  * @brief Range
+ *
+ * The code snippet defines a struct template called `Range` that represents a
+ * range of values. It has two template parameters, `T` which represents the
+ * type of the values in the range.
  *
  * @tparam T
  */
@@ -104,21 +135,36 @@ public:
   T stop;
 
   /**
-   * @brief
+   * @brief begin
+   *
+   * The `begin()` function is a member function of the `Range` struct
+   * template. It returns an iterator pointing to the beginning of the range. In
+   * this case, it creates a `RangeIterator` object with the `start` value of
+   * the range and returns it.
    *
    * @return iterator
    */
   constexpr auto begin() const -> iterator { return iterator{this->start}; }
 
   /**
-   * @brief
+   * @brief end
+   *
+   * The `end()` function is a member function of the `Range` struct template.
+   * It returns an iterator pointing to the end of the range. In this case, it
+   * creates a `RangeIterator` object with the `stop` value of the range and
+   * returns it. This iterator represents the position one past the last element
+   * in the range.
    *
    * @return iterator
    */
   constexpr auto end() const -> iterator { return iterator{this->stop}; }
 
   /**
-   * @brief
+   * @brief empty
+   *
+   * The `empty()` function is a member function of the `Range` struct template.
+   * It checks if the range is empty, meaning if the `start` and `stop` values
+   * are equal.
    *
    * @return true
    * @return false
@@ -126,7 +172,11 @@ public:
   constexpr auto empty() const -> bool { return this->stop == this->start; }
 
   /**
-   * @brief
+   * @brief size
+   *
+   * The `size()` function is a member function of the `Range` struct template.
+   * It calculates and returns the size of the range, which is the number of
+   * elements in the range.
    *
    * @return size_t
    */
